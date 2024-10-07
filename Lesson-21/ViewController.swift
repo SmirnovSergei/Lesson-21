29a313648a6cf43ce1559c4f2503c7ca2327c36b
Smirnov Sergey <fost@live.ru>
2024-10-07 23:03:04 +0300

    Добавление файлов в репозиторий

---
+++ Lesson-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-21
+//
+//  Created by Сергей Смирнов on 07.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
